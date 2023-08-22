86c77afeede12b4fae2de73137b9c9fe52975317
Ralph <129913847+LopezAnthonyRalph@users.noreply.github.com>
2023-08-22 14:26:30 -0700

    Add files via upload

---
+++ src/links.js
@@ -0,0 +1,50 @@
+export const mainContent = {
+  // imgSrc: `/assets/nc.png`,
+  name: `@ Next Corner`,
+  tagline: ``,
+};
+
+
+
+export const socialLinks = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: '',
+  },
+  {
+    title: 'Linkedin',
+    icon: 'mdi:linkedin  ',
+    url: '',
+  },
+  {
+    title: 'Discord',
+    icon: 'mdi:discord  ',
+    url: '',
+  },
+  {
+    title: 'Website',
+    icon: 'mdi:globe',
+    url: '',
+  }
+]
+
+export const links = [
+  {
+    text: 'Home',
+    url: '',
+    featured: true,
+  },
+  {
+    text: 'About',
+    url: '',
+  },
+  {
+    text: 'Uses',
+    url: '',
+  },
+  {
+    text: 'See my latest blog post!',
+    url: '',
+  }
+]
